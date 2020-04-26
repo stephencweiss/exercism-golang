198f086a19b3e14aa9645888060dddaf1e364365
Stephen <stephen.c.weiss@gmail.com>
2020-04-26 12:59:08 -0500

    feat: raindrops

---
+++ raindrops/raindrops.go
@@ -0,0 +1,23 @@
+// Package raindrops returns the sound made by a number of raindrops.
+package raindrops
+
+import "strconv"
+
+// Convert is the function that takes an integer of rain drops and converts it to its noise equivalent.
+// Noises are only emitted from raindrop counts that are factors of 3,5, or 7.
+func Convert(raindropCount int) string {
+	noise := ""
+	if 0 == raindropCount%3 {
+		noise = noise + "Pling"
+	}
+	if 0 == raindropCount%5 {
+		noise = noise + "Plang"
+	}
+	if 0 == raindropCount%7 {
+		noise = noise + "Plong"
+	}
+	if noise == "" {
+		noise = strconv.Itoa(raindropCount)
+	}
+	return noise
+}
